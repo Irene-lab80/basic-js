f8751aeabd8e1af901296a0401e21aacd83332ad
irene-lab80 <you@example.com>
2021-03-29 17:14:41 +0300

    init: start carbon, chain and array

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,13 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if ((typeof sampleActivity == 'string') && (parseFloat(sampleActivity) > 0)){
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    let log1 = Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity));
+    var t = Math.ceil(log1 / k);
+    return t;
+  } else {
+    return false;
+  }
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,50 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  let controls = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+  if (Array.isArray(arr)) {
+    tempArr = arr.map((el) => el);
+    for (let i = 0; i < tempArr.length; i++) {
+      if (tempArr[i] === '--discard-next') {
+        if (tempArr[i + 1] !== undefined) {
+          if (!controls.includes(tempArr[i + 1])) {
+            tempArr.splice(i + 1, 1);
+          }
+        }
+      }
+      else if (tempArr[i] === '--discard-prev') {
+        if (tempArr[i - 1] !== undefined) {
+          if (!controls.includes(tempArr[i - 1])) {
+            tempArr.splice(i - 1, 1);
+            i++;
+          }
+        }
+      }
+      else if (tempArr[i] === '--double-next') {
+        if (tempArr[i + 1] !== undefined) {
+          if (!controls.includes(tempArr[i + 1])) {
+            tempArr.splice(i + 1, 0, tempArr[i + 1]);
+            i++;
+          }
+        }
+      }
+      else if (tempArr[i] === '--double-prev') {
+        if (tempArr[i - 1] !== undefined) {
+          if (!controls.includes(tempArr[i - 1])) {
+            tempArr.splice(i - 1, 0, tempArr[i - 1]);
+            i++;
+          }
+        }
+      }
+    }
+  }
+  else {
+    throw new Error();
+  }
+  for (let i = 0; i < tempArr.length; i++) {
+    if (controls.includes(tempArr[i])) {
+      tempArr.splice(i, 1);
+      i--;
+    }
+  }
+  return tempArr;};
